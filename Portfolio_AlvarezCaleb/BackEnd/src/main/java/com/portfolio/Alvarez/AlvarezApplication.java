24b2620b80033398e7136bc3a6fce42a6fabd1a5
calv01 <110236321+calv01@users.noreply.github.com>
2022-10-24 14:39:21 -0300

    Creación de carpeta BackEnd
    Se crea la carpeta que contiene el BackEnd

---
+++ Portfolio_AlvarezCaleb/BackEnd/src/main/java/com/portfolio/Alvarez/AlvarezApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.Alvarez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlvarezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlvarezApplication.class, args);
+	}
+
+}
---
+++ Portfolio_AlvarezCaleb/BackEnd/src/test/java/com/portfolio/Alvarez/AlvarezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.Alvarez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlvarezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
