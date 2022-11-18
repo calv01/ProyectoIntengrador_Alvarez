98c2fc803b507690ab491ab59c968bcfea16a750
calv01 <110236321+calv01@users.noreply.github.com>
2022-11-18 11:30:56 -0300

    deploy

--- Portfolio_AlvarezCaleb/BackEnd/src/main/java/com/portfolio/calvarez/Controller/CExperiencia.java
+++ Portfolio_AlvarezCaleb/BackEnd/src/main/java/com/portfolio/calvarez/Controller/CExperiencia.java
@@ -27,7 +27,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("explab")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://frontendalvarez.web.app/")
 public class CExperiencia {
     @Autowired
     SExperiencia sExperiencia;
--- Portfolio_AlvarezCaleb/FrontEnd/src/app/service/persona.service.ts
+++ Portfolio_AlvarezCaleb/FrontEnd/src/app/service/persona.service.ts
@@ -9,7 +9,7 @@ import { persona } from '../model/persona.model';
   providedIn: 'root'
 })
 export class PersonaService {
-  URL = 'http://localhost:8080/personas/';
+  URL = 'https://bkdalvarez.herokuapp.com/personas/';
 
   constructor(private http: HttpClient) { }
   public getPersona(): Observable<persona>{
