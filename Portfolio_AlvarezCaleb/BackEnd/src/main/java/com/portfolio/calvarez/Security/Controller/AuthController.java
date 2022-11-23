47b34f369d6aab24ec530f7b1581fab8ff71ee9d
calv01 <110236321+calv01@users.noreply.github.com>
2022-11-23 16:34:34 -0300

    ...

--- Portfolio_AlvarezCaleb/BackEnd/src/main/java/com/portfolio/calvarez/Security/Controller/AuthController.java
+++ Portfolio_AlvarezCaleb/BackEnd/src/main/java/com/portfolio/calvarez/Security/Controller/AuthController.java
@@ -36,7 +36,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/auth")
 //@CrossOrigin(origins = "http://localhost:4200")
-@CrossOrigin(origins = "http://frontendalvarez.web.app")
+@CrossOrigin(origins = "https://frontendalvarez.web.app")
 public class AuthController {
     @Autowired
     PasswordEncoder passwordEncoder;
